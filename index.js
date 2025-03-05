a266f323f29f9b25ee1993d4ecf7e46e6654fc70
Lawrence-wambugu <wambugulawrence@gmail.com>
2025-03-05 11:58:48 +0300

    Edits index.js

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+const num1 = 31;  
+const num2 = 2;   
+
+const multiply = num1 * num2;  
+
+console.log(multiply);
+
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+console.log(random);
+
+let numbers = [10, 5, 15, 20, 8];
+let max = Math.max(...numbers);
+console.log(max);  // This will log 20
+
+const num3 = 14;  
+const num4 = 5;   
+
+const mod = num3 % num4;  
+
+console.log(mod);
